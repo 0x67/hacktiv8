3c2a1f0e5bceeceed93b955bab8eb5329ebe9280
GabrielPutra <gabri.putra@gmail.com>
2020-09-04 03:23:00 +0700

    Added ubahHuruf.js (only vocal char 'a' and 'i in this version). And fixing palindromAngka.js

--- phase0/w1/d5/palindromeAngka.js
+++ phase0/w1/d5/palindromeAngka.js
@@ -6,14 +6,13 @@ let balik = ''
 //console.log(str + ' ' + typeof str)
 
 for (var i = angka; i >= angka; i++) {
-    
     let str = i.toString()
     //console.log(str + ' adalah ' + typeof str + ' ini i')
     //console.log(str.length)
     for (var j = str.length - 1; j >= 0; j--) {
-        balik += str[j] 
-        
+        balik += str[j] // iterates balik with value equal to j
     }
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/break
     // check if balik is equal to str
     if (balik !== str) {    // if not equal
         balik = ''          // empty the variable to store new value
@@ -22,8 +21,6 @@ for (var i = angka; i >= angka; i++) {
     if (balik === str) {    // if equal
         break               // stop the loop
     }
-    
-    
 }
 angka = parseInt(balik) // if output require number 
 console.log(parseInt(angka) + ' ini balik') // if output accept 'string' as number then just change >> balik
---
+++ phase0/w1/weekend/ubahHuruf.js
@@ -0,0 +1,31 @@
+/* requirements
+var kata = 'i love javascript'
+
+maka output yang dihasilkan adalah '$ l$v$ j$v$scr$pt'
+*/
+
+let kata = 'abiiiiiee'
+// let vokal = ['a', 'i','u','e','o'] //idk how to use array so i comment this
+let output = ''
+
+
+for (var i = 0; i <= kata.length; i++) {
+    // output += kata[i] // iterates output with value of kata >> if kata[0] >> output = a
+    var outputVokal = ''
+    if (kata[i] == 'a' || kata[i] == 'i') {  // if output equal to 'vokal'
+        //output = ganti
+        outputVokal = '$'
+        output += outputVokal   // change to ganti in the output[length] 
+        //output += kata[i+1]
+        //add non vokal character from 'kata' to 'output'
+        continue;
+    }
+    else if (outputVokal = kata[i]){
+        
+        output = output + outputVokal
+    }
+    
+    //console.log(output)
+    //console.log(vokal)
+}
+console.log(output + " kalo vokal")
