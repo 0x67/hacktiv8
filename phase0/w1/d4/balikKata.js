cddff29fa7292fde726277b269ae21f9b5a14917
GabrielPutra <gabri.putra@gmail.com>
2020-09-04 01:36:30 +0700

    Re-push balikKata.js, wrong push earlier

--- phase0/w1/d4/balikKata.js
+++ phase0/w1/d4/balikKata.js
@@ -1,6 +1,25 @@
-let kata = 'abi';
+let kata = 'Javascript'
+let balik = '' // empty string to display reversed string
 
-for (var i = 0; i < strBitCount.length; ++i) {
-  if (Number(strBitCount[i]) === 1)
-    answer++;
+// the loop
+// start is (kata.length - 1) which is 't', as as i is greater than 0 i will continue
+// decrement after each loop
+for (var i = kata.length - 1; i >= 0; i--) {
+    balik += kata[i]
+    /*  Javascript length is equal to 10
+    first loop  >> i = 10 - 1 = 9, balik = "" + "t" = "t"
+    second loop >> i = 10 - 2 = 8, balik = "t" + "p" = "tp"
+    ...and so on ...
+    */
 }
+console.log(balik)
+
+/* or we can do it this way instead
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/values
+let kata = 'Javascript';
+let balik = "";
+for (let value of kata) {
+  balik = value + balik;
+}
+console.log(balik);
+*/
