218467768f7342310c54d27f6e14a36a6a843af7
GabrielPutra <gabri.putra@gmail.com>
2020-09-05 17:54:47 +0700

    Added (if vocalChar = isUpperCase) change to specialChar

--- phase0/w1/weekend/ubahHuruf.js
+++ phase0/w1/weekend/ubahHuruf.js
@@ -1,17 +1,27 @@
 /* requirements
 var kata = 'i love javascript'
-
 maka output yang dihasilkan adalah '$ l$v$ j$v$scr$pt'
+var kata = 'I LoVE JAvaSCRipt'
+maka output yang dihasilkan adalah '$ L$V$ J$v$SCR$pt
 */
 
-let kata = 'aiueo IS VOCAL'
+let kata = 'I LoVE JAvaSCRipt'
 // let vokal = ['a', 'i','u','e','o'] //idk how to use array so i comment this
 let output = ''
 
 
 for (var i = 0; i <= kata.length; i++) {
     // output += kata[i] // iterates output with value of kata >> if kata[0] >> output = a
-    var outputVokal = ''
+    var outputVokal = '$'
+    if (kata[i] == 'a'.toUpperCase() || kata[i] == 'i'.toUpperCase() || kata[i] == 'u'.toUpperCase() || kata[i] == 'e'.toUpperCase() || kata[i] == 'o'.toUpperCase()) {  // if output equal to 'vokal'
+        //output = ganti
+
+        output += outputVokal   // change to ganti in the output[length] 
+        //output += kata[i+1]
+        //add non vokal character from 'kata' to 'output'
+        continue;
+        
+    }
     if (kata[i] == 'a' || kata[i] == 'i' || kata[i] == 'u' || kata[i] == 'e' || kata[i] == 'o') {  // if output equal to 'vokal'
         //output = ganti
         outputVokal = '$'
@@ -28,4 +38,5 @@ for (var i = 0; i <= kata.length; i++) {
     //console.log(output)
     //console.log(vokal)
 }
-console.log(output + " kalo vokal")
+console.log(kata + ' = input')
+console.log(output + ' = output')
