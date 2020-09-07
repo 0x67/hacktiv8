1bac3fbe8af2bb6a3f5154b3325b5d90d051fe6a
0x67 <gabri.putra@gmail.com>
2020-09-08 02:21:57 +0700

    Added early version of ganjilGenap

--- phase0/w2/d2/test.js
+++ phase0/w2/d2/test.js
@@ -1,11 +1,38 @@
 
 // GANJIL GENAP => JUMLAH PLAT GANJIL ATAU GENAP
 
-var plat = ''
+var plat = '864;1308;1276;1432'
 var platSplit = plat.split(';')
 var input = ''
+var genap = 0
+var ganjil = 0
 function ganjilGenap() {
-    str = 'TEST STRING'
-    console.log(str)
+    for (var i = 0; i < platSplit.length; i++) {
+        if (platSplit[i] % 2 == 0) {
+            genap += 1 
+
+        }
+        else if (platSplit[i] % 2 != 0){
+            ganjil++
+        }
+        
+    }
+    if (ganjil > 0 && genap > 0) {
+        console.log(`plat ganjil: ${ganjil} dan plat genap: ${genap}`)
+    }
+    else if (ganjil > 0 && genap == 0) {
+        console.log(`plat ganjil: ${ganjil} dan plat genap tidak ditemukan`)
+    }
+
+    else if (ganjil == 0 && genap > 0) {
+        console.log(`plat genap: ${genap} dan plat ganjil tidak ditemukan`)
+    }
+    else if (ganjil == 0 && genap == 0) {
+        console.log('plat tidak ditemukan')
+    }
+    else {
+        console.log('invalid data')   
+    }        
 }
+
 ganjilGenap()
