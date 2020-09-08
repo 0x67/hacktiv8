c9a193407eb4c73ad5e4f0cc2b96a6968c234c15
0x67 <gabri.putra@gmail.com>
2020-09-09 06:29:45 +0700

    re-commit

--- phase0/w2/d3/duduk.js
+++ phase0/w2/d3/duduk.js
@@ -3,12 +3,17 @@ function sittingArrangement(person, column) {
         return 'invalid number'
     }
     const personSplit = [];
+    const kosong = ['Kosong']
     for (let i = 0; i < person.length; i++) {
         const lastPerson = personSplit[personSplit.length - 1];
         if (!lastPerson || lastPerson.length === column) {
             personSplit.push([person[i]]);
         } else {
             lastPerson.push(person[i]);
+        }
+
+        if (!personSplit && lastPerson && lastPerson.length > column) {
+            lastPerson[i].push(kosong)
         }
     }
     return personSplit;
