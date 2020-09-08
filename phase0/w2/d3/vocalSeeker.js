abbc514b05632220460150c61254d2bdcb805086
0x67 <gabri.putra@gmail.com>
2020-09-09 02:10:23 +0700

    Basically done but still not efficient, need some minifying

--- phase0/w2/d3/vocalSeeker.js
+++ phase0/w2/d3/vocalSeeker.js
@@ -2,11 +2,19 @@ let board = [
     ['*', '*', '*', 10],
     ['*', '*', -5, -10, '*', 100],
     ['a', 'A', 'o', 'b']
-  ]
-
+]
+//let vocal = 'aAiIuUeEoO'
+let output = 0
+let vokal = ''
+///////////////////////////
 for (var i = 0; i < board.length; i++) {
     var boards = board[i];
-    for (var j = 0; j < board.length; j++) {
-        console.log(`board [${i}][${j}] = ${board[j]}`)
+    for (var j = 0; j < boards.length; j++) {
+        if (boards[j] === 'a' || boards[j] == 'i' || boards[j] == 'u' || boards[j] == 'e' || boards[j] == 'o' ||
+            boards[j] == 'a'.toUpperCase() || boards[j] == 'i'.toUpperCase() || boards[j] == 'u'.toUpperCase() || boards[j] == 'e'.toUpperCase() || boards[j] == 'o'.toUpperCase()) {
+            output += 1
+            vokal += boards[j]
+        }
     }
 }
+console.log(`vokal ditemukan ${output} dan kumpulan vokal adalah ${vokal}`)
