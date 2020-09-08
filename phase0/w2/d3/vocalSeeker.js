76b3066b1bdfd65fbddd7c6f8d339ca7fae85b3b
0x67 <gabri.putra@gmail.com>
2020-09-09 02:13:02 +0700

    Change to function

--- phase0/w2/d3/vocalSeeker.js
+++ phase0/w2/d3/vocalSeeker.js
@@ -1,20 +1,23 @@
+//let vocal = 'aAiIuUeEoO'
+function vocalSeeker(board) {
+    let output = 0
+    let vokal = ''
+    for (var i = 0; i < board.length; i++) {
+        var boards = board[i];
+        for (var j = 0; j < boards.length; j++) {
+            if (boards[j] === 'a' || boards[j] == 'i' || boards[j] == 'u' || boards[j] == 'e' || boards[j] == 'o' ||
+                boards[j] == 'a'.toUpperCase() || boards[j] == 'i'.toUpperCase() || boards[j] == 'u'.toUpperCase() || boards[j] == 'e'.toUpperCase() || boards[j] == 'o'.toUpperCase()) {
+                output += 1
+                vokal += boards[j]
+            }
+        }
+    }
+    return `vokal ditemukan ${output} dan kumpulan vokal adalah ${vokal}`
+}
+
 let board = [
     ['*', '*', '*', 10],
     ['*', '*', -5, -10, '*', 100],
     ['a', 'A', 'o', 'b']
 ]
-//let vocal = 'aAiIuUeEoO'
-let output = 0
-let vokal = ''
-///////////////////////////
-for (var i = 0; i < board.length; i++) {
-    var boards = board[i];
-    for (var j = 0; j < boards.length; j++) {
-        if (boards[j] === 'a' || boards[j] == 'i' || boards[j] == 'u' || boards[j] == 'e' || boards[j] == 'o' ||
-            boards[j] == 'a'.toUpperCase() || boards[j] == 'i'.toUpperCase() || boards[j] == 'u'.toUpperCase() || boards[j] == 'e'.toUpperCase() || boards[j] == 'o'.toUpperCase()) {
-            output += 1
-            vokal += boards[j]
-        }
-    }
-}
-console.log(`vokal ditemukan ${output} dan kumpulan vokal adalah ${vokal}`)
+console.log(vocalSeeker(board))
