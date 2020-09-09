04684a79cfebe527cde5c6b3471349688dd7aedc
0x67 <gabri.putra@gmail.com>
2020-09-10 02:10:32 +0700

    Only works if 'o' is found before 'x'

--- phase0/w2/d4/targetTerdekat.js
+++ phase0/w2/d4/targetTerdekat.js
@@ -1,30 +1,32 @@
-let arr = [' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'] // O = 2. X = 7
-let indeX = 0
-let indexO = 0
-let count = 0
-let arrSplit = []
+function targetTerdekat(arr) {
+    let xIndex = 0
+    let oIndex = 0
+    let arrSplit = []
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 'o') {
-        indexO = i
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'o') {
+            oIndex = i
+        } else if (arr[i] === 'x') {
+            xIndex = i
+            break
+        }
     }
-    if (arr[i] === 'x') {
-        indexX = i
-        break
-    }
-}
 
-for (let j = indexO; j <= indexX; j++) {
-    arrSplit.push(arr[j])
-}
-console.log(indexX)
-console.log(indexO)
-console.log(arrSplit)
-let jarak = arrSplit.length - 1
-console.log(`Jarak terdekat adalah: ${jarak}`)
-// TEST CASES
-//console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
-//console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-//console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
-//console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-//console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+    for (let j = oIndex; j <= xIndex; j++) { // split the arr from oIndex to xIndex
+        arrSplit.push(arr[j])
+    }
+
+    //let jarak = arrSplit.length - 1
+    let jarak = xIndex - oIndex
+    //console.log(oIndex)
+    //console.log(xIndex)
+    //console.log(arrSplit)
+    return `Jarak terdekat antara 'X' dan 'O': ${jarak}`
+  }
+  
+  // TEST CASES
+  console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+  console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+  //console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+  //console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+  //console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
