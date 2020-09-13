0af11f465e69a16c88883030031fe12fa5995ad4
0x67 <gabri.putra@gmail.com>
2020-09-13 20:32:08 +0700

    Find the occurence of 'abc' in a string

---
+++ phase0/customChallenges/cariPelaku.js
@@ -0,0 +1,16 @@
+function cariPelaku(str) {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' && str[i + 1] === 'b' && str[i + 2] === 'c') {
+            count++
+        }
+    }
+    return count
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
