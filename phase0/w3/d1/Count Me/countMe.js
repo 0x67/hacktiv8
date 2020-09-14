2cb83137ab83b521933b8399472ee1bd909e2de4
0x67 <gabri.putra@gmail.com>
2020-09-14 18:18:23 +0700

    Added CountMe on W3D1

--- phase0/w3/d1/Count Me/countMe.js
+++ phase0/w3/d1/Count Me/countMe.js
@@ -1,22 +1,21 @@
 function countMe(arr) {
-    var a = [], b = [], prev;
+    let obj = {}
 
-    arr.sort();
-    for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i] !== prev ) {
-            a.push(arr[i]);
-            b.push(1);
-        } else {
-            b[b.length-1]++;
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]] == undefined) {
+            obj[arr[i]] = 0
         }
-        prev = arr[i];
-    }
 
-    return [a, b];
+        obj[arr[i]] += 1
+
+    }
+    return obj
 }
-  
-   console.log(countMe(['Sofyan', 'Ricky', 'Sofyan', 'Semmi', 'Semmi', 'Wika']));
-  // { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
-  
-console.log(countMe([ 1, 15, 9, 10, 8, 1, 12, 15, 10, 3 ]));
-  // { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
+
+console.log(countMe(['Sofyan', 'Ricky', 'Sofyan', 'Semmi', 'Semmi', 'Wika']))
+// { Sofyan: 2, Ricky: 1, Semmi: 2, Wika: 1 }
+
+console.log(countMe([1, 15, 9, 10, 8, 1, 12, 15, 10, 3]))
+// { '1': 2, '3': 1, '8': 1, '9': 1, '10': 2, '12': 1, '15': 2 }
+
+module.exports = countMe
