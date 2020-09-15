0b2ac3a5ad86ea17e099352f4e8d0488ad5978c1
0x67 <gabri.putra@gmail.com>
2020-09-15 16:56:32 +0700

    Soal latihan buatPukis

--- phase0/w3/d2/latihan/buatpukis.js
+++ phase0/w3/d2/latihan/buatpukis.js
@@ -23,8 +23,46 @@ ketentuan:
 
 
 function buatPukis(ingredients){
-    
+    if(!ingredients.length){
+        return 'Tidak ada bahan untuk membuat kue pukis';
+    }
+    let result = {}
+    const ingredientsNeeded = {
+        flour: 100,
+        egg: 0.25,
+        sugar: 50,
+        chocolate: 20
+    }
+    let objingredients = {}
+    for(let i = 0; i < ingredients.length; i++){
+        let keys = ingredients[i][0];
+        let values = ingredients[i][1];
+        objingredients[keys] = values;
+    }
 
+    let numberofpukis = Number.MAX_VALUE;
+    let temp = 0;
+    for(const keys in ingredientsNeeded){
+        if(!objingredients[keys]){
+            return 'Bahan - bahan tidak lengkap untuk membuat kue pukis';
+        }
+        temp = Math.floor(objingredients[keys] / ingredientsNeeded[keys]);
+        if(temp < numberofpukis){
+            numberofpukis = temp;
+        }
+    }
+
+    for(const keys in objingredients){
+        let usedingredients = ingredientsNeeded[keys] * numberofpukis;
+        objingredients[keys] = objingredients[keys] - usedingredients;
+        if(objingredients[keys] === 0){
+            delete objingredients[keys];
+        }
+    }
+
+    result.kuepukis = numberofpukis;
+    result.sisa = objingredients;
+    return result;
 }
 
 console.log(buatPukis([["flour", 1000], ["egg", 5], ["sugar", 400], ["chocolate", 200]]));
