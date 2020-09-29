421d6c9ec54f4b49a99cde99401ed1ff81b61eb1
0x67 <gabri.putra@gmail.com>
2020-09-29 23:51:11 +0700

    Fixed checkVowel function

--- phase1/w1/d3/checkVowelArray/vowelArray.js
+++ phase1/w1/d3/checkVowelArray/vowelArray.js
@@ -1,10 +1,40 @@
+// release 1
+function checkVowel(row, column) {
+    let arr = generateArray(row, column)
+    console.log(arr)
+    let vocal = 'AIUEO'
+    let countBlock = 0
+    
+    for (let i = 0; i < row - 1; i++) {
+        for (let j = 0; j < column - 1; j++) {
+            let count = 0
+            if (vocal.includes(arr[i][j])) {
+                count++
+            }
+            if (vocal.includes(arr[i][j + 1])) {
+                count++
+            }
+            if (vocal.includes(arr[i + 1][j])) {
+                count++
+            }
+            if (vocal.includes(arr[i + 1][j + 1])) {
+                count++
+            }
+            if (count === 4) {
+                countBlock++
+            }
+        }
+    }
+    return countBlock
+}
+
 // release 0
 function generateArray(row, column) {
     let output = []
     let kamus = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
-    
+
     for (let i = 0; i < row; i++) {
         let temp = []
         for (let j = 0; j < column; j++) {
@@ -17,25 +47,13 @@ function generateArray(row, column) {
     return output
 }
 
-// release 1
-function checkVowel(arr) {
-    let vocal = 'AIUEO'
-    let count = 0
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (vocal.includes(arr[i][j]) && vocal.includes(arr[i][j + 1]) && vocal.includes(arr[i + 1][j]) && vocal.includes(arr[i + 1][j + 1])) {
-                count++
-            }
-        }
-    }
-    return count
-}
-console.log(generateArray(5, 4))
+console.log(checkVowel(5, 4))
 
-console.log(checkVowel([
-    [ 'N', 'W', 'L', 'N' ],
-    [ 'E', 'O', 'F', 'A' ],
-    [ 'U', 'I', 'L', 'S' ],
-    [ 'K', 'O', 'Z', 'H' ],
-    [ 'R', 'S', 'Y', 'W' ]
-  ]))
+// console.log(checkVowel([
+//     ['N', 'W', 'L', 'N'],
+//     ['E', 'O', 'F', 'A'],
+//     ['U', 'I', 'L', 'S'],
+//     ['K', 'O', 'A', 'I'],
+//     ['R', 'S', 'E', 'A']
+// ])) // 2
+
