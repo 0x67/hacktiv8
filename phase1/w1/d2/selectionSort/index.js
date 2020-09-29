5cf07b2f36fd1aa37b7586f48b77472a8780d209
0x67 <gabri.putra@gmail.com>
2020-09-30 00:02:18 +0700

    Added selectionSort

---
+++ phase1/w1/d2/selectionSort/index.js
@@ -0,0 +1,27 @@
+function selectionSort(arr) {
+    let len = arr.length
+
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j
+            }
+        }
+        if (min !== i) {
+            let temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        }
+    }
+    return arr
+}
+
+console.log(selectionSort([33, 2, 52, 106, 73]));
+// [ 2, 33, 52, 73, 106 ]
+
+console.log(selectionSort([13, 5, 22, 99, 11]));
+// [ 5, 11, 13, 22, 99 ]
+
+// abaikan code dibawah
+module.exports = selectionSort;
